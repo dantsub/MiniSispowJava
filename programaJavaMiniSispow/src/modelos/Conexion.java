9f65e6f1bafab6b1d1b5afc6fe60c24fff6a08d5
SENA <SENA@CALSISSBT7F025.FormacionSena.RED>
2018-09-05 17:08:34 -0500

    update

--- programaJavaMiniSispow/src/modelos/Conexion.java
+++ programaJavaMiniSispow/src/modelos/Conexion.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package app;
+package modelos;
+import modelos.*;
 import java.awt.HeadlessException;
 import java.sql.*;
 
